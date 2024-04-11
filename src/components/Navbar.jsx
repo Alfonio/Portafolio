3ffef134d6e64f25638ee7159800d92e9d6acfe7
Alfonso Ferraro <alfosotff8903@gmail.com>
2024-04-11 17:41:49 -0300

    Cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="mb-20 flex items-center justify-between py-6 px-0">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-0 w-40 hy-20" src={logotipo} alt="logo" />
+        <img className="mx-0 w-40 hy-20 px-4" src={logotipo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin/>
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,36 +6,36 @@ import project4 from "../assets/projects/project-4.jpg";
 export const HERO_CONTENT = `¡Hola! Tengo 20 años y estoy emprendiendo en el camino de la ingeniería informática. Por otro lado, soy desarrollador front-end y estudio Data Science. Los conocimientos que he logrado hasta ahora incluyen HTML, CSS, Bootstrap, Sass, JavaScript, 
 ReactJS, C, Python y tengo algunas habilidades con Tailwind y Next.js.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `Soy un estudiante de Ingeniería Informática de 20 años especializado en desarrollo Front-End. Tengo habilidades técnicas en HTML, CSS, Bootstrap, Sass, JavaScript y experiencia en ReactJS. He adquirido algunos conocimientos en Ruby, ESLint y Node.js. Para el estudio de bases de datos implemento SQL/SQLite3, .xlsx y .csv. Ya sea para Data Science-Analyst o Machine Learning utilizando Python. Últimamente estoy metido en Power BI para agilizar mi desempeño en la visualización y la multiplicidad de bases relacionales. Por último, en base a mis estudios actuales, estoy inmerso en el estudio de Tailwind para agilizar la escritura de mi CSS y con Next.js para crear aplicaciones web de alto rendimiento.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2024 - Presente",
+    role: "Aprendizaje de nuevas tecnologías",
+    company: "Tailwind & Next.js",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+    technologies: ["Prisma", "React.js", "Next.js", "Tailwind"],
   },
   {
-    year: "2022 - 2023",
+    year: "2023 - 2024 Data",
+    role: "Data Science / Analytics",
+    company: "Oracle & IBM",
+    description: `En mi programa de bootcamp de programador web, me dieron la opción de continuar mis estudios para obtener una formación más completa. Con gusto acepté y decidí seguir mi formación como Profesional de Data Science en el programa de Alura-Oracle. Después de completar este programa y enfocarme en mis estudios universitarios, retomé mi formación previamente con el programa de IBM, donde se hace un repaso de Data Science y se profundiza en términos de Data Analysis para el estudio de datos matemáticos y métricas visualizables.`,
+    technologies: ["Python", "Pandas", "Seaborn", "Scikit-learn", "Base de datos"],
+  },
+  {
+    year: "2022 - 2023 Front-end",
     role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    company: "Bootcamps y Estudios",
+    description: `Fue mi base y mi inspiración, la parte en la que he invertido más trabajo duro y esfuerzo para aprender nuevas tecnologías. He participado en varios bootcamps y programas donde he adquirido un amplio conocimiento de tecnologías y frameworks que utilizo a diario para crear aplicaciones. Mi desarrollo como Front-End developer ha sido resultado de estudios en diferentes empresas como Google, Alura-Oracle, OpenBootcamp, el Ministerio de Educación (AR-Programa 2023), Telefónica, entre otros...`,
+    technologies: ["HTML", "CSS", "Javascript","React.js","SQL", "Bootstrap","Git", "..."],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
+    year: "2020 - 2021 Nacimiento",
     role: "Software Engineer",
     company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    description: `Dicen que "Los primeros pasos de un programador suelen estar llenos de curiosidad y entusiasmo", y en mi caso, eso fue exactamente lo que sucedió. Motivado por el deseo de crear tecnología para resolver los problemas cotidianos de las personas, comencé investigando en videos en internet y dando mis primeros pasos con HTML y CSS. Después de unos meses, tomé la decisión final al terminar la escuela secundaria: me inscribí en ingeniería informática y comencé a trazar mi futuro como una persona ejemplar que pueda ayudar al prójimo con mis creaciones tecnológicas`,
+    technologies: ["HTML", "CSS", "C", "PASCAL"],
   },
 ];
 
@@ -72,6 +72,6 @@ export const PROJECTS = [
 
 export const CONTACT = {
   address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
+  phoneNo: "+54 223 547-4874",
   email: "me@example.com",
 };
